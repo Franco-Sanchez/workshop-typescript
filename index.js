f2abe4b496f00696c18ded2422ffe7a060106987
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-06-06 18:45:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var x = "my string";
+x = "hola";
+x = 2;
+var y = "A CONSTANT";
+y = "OTHER";
+var myvar;
+var _aNumber;
+var aNumber = 2;
+var aBoolean = true;
---
+++ index.ts
@@ -0,0 +1,13 @@
+let x = "my string";
+x = "hola";
+x = 2;
+
+const y = "A CONSTANT";
+y = "OTHER";
+
+let myvar;
+
+let _aNumber: number;
+let aNumber = 2;
+
+let aBoolean = true;
